ba928f7779f0db3326c584f97ccb1a7ffe478c8f
M.Syamsandi <zxvcklh@gmail.com>
2025-03-17 10:52:46 +0700

    init microservice project

---
+++ services/confiig-server/src/test/java/com/syamsandi/confiig/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syamsandi.confiig.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
